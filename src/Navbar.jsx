6a60326f08aeb42c691682160e7789a6b1ffc0c9
DerekRivera2332 <derek24p@gmail.com>
2025-11-19 23:12:58 -0600

    Se arma el navbar
    Se crea el Navbar con sus funcionalidades, ademas se crea el (navbar.css) este ultimo se encarga de darle toda la parte grafica del navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,1 +1,32 @@
-import React from "react";  
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="navbar-left">
+                <img
+                    className="logo nodejs"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png"
+                    alt="Logo Node.js"
+                />
+
+                <ul className="navbar-menu">
+                    <li>Aprender</li>
+                    <li>Acerca de</li>
+                    <li>Descargar</li>
+                    <li>Blog</li>
+                    <li>Documentación</li>
+                    <li>Contribución ↗</li>
+                    <li>Certificación ↗</li>
+                </ul>
+            </div>
+
+            <div className="buscador">
+                <input type="text" placeholder="Buscar..." />
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
